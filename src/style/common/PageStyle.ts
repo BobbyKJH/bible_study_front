108c850c47c2c2d1e82084df8ca26b676e5e364c
bobby <rlatnguscjsw@gmail.com>
2023-08-21 22:20:01 +0900

    Style : PageStyle
    - Page 크기가 900px이하로 가면 margin left 0으로 바꾸준다

--- src/style/common/PageStyle.ts
+++ src/style/common/PageStyle.ts
@@ -1,5 +1,8 @@
 import styles from "styled-components"
 
 export const PageContainer = styles.div`
-		margin-left: 240px;
+		margin-left: 200px;
+		@media (max-width: 900px) {
+		  margin-left: 0
+		}
 	`
