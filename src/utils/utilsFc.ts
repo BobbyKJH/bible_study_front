83705377cfc0658c09bfde76cd8e61bcfdd77550
bobby <rlatnguscjsw@gmail.com>
2023-08-25 22:50:31 +0900

    Refactor : pathLocation
    - pathLocation utils Function return 값 변경

--- src/utils/utilsFc.ts
+++ src/utils/utilsFc.ts
@@ -1,7 +1,11 @@
 import { useLocation } from 'react-router'
 
-export const pathLocaiton = (path: string): boolean => {
+export const pathLocaiton = (path: string): { backgroundColor: string } => {
 	const { pathname } = useLocation();
 
-	return path === "" ? pathname === "/" : pathname.includes(path);
+	if(path === "") {
+		return pathname === "/" ? { backgroundColor: "rgba(0, 0, 0, 0.04)"} :  { backgroundColor: "#fff"}
+	}
+
+	return pathname.includes(path) ? { backgroundColor: "rgba(0, 0, 0, 0.04)"} :  { backgroundColor: "#fff"};
 }
