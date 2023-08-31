dbab13562ff27e982ba9d75dc852f8d3a0135ea7
bobby <rlatnguscjsw@gmail.com>
2023-08-31 21:39:54 +0900

    Feat : 성경 신약 구약 페이지
    - 성경 신약 구약 페이지 생성

---
+++ src/page/bible/BibleOldTestamentPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageContainer } from '@style/common/PageStyle.ts'
+
+const BibleOldTestamentPage: React.FC = () => {
+	return (
+		<PageContainer>
+			구약
+		</PageContainer>
+	);
+};
+
+export default BibleOldTestamentPage;
