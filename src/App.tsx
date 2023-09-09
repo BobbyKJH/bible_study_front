fe9b0713317effab63a381ceb0c63dde62744bf2
bobby <rlatnguscjsw@gmail.com>
2023-09-09 22:50:29 +0900

    Refactor : main -> app
    - main -> app BrowserRouter GlobalStyle 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
 /** React */
 import React from 'react'
 import { Route, Routes } from "react-router";
+import { BrowserRouter } from 'react-router-dom'
 /** Tanstack-Query */
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
-/** Page */
-import HomePage from '@/page/HomePage.tsx'
+/** Components */
 import SideBar from '@components/common/sideBar/SideBar.tsx'
 import Header from '@components/common/Header.tsx'
-import BiblePage from '@page/BiblePage.tsx'
-import PBSPage from '@page/PBSPage.tsx'
-import QTPage from '@page/QTPage.tsx'
-import QTCreatePage from '@page/QTCreatePage.tsx'
-import PBSCreatePage from '@page/PBSCreatePage.tsx'
+/** Page */
+import HomePage from '@/page/HomePage.tsx'
+import BibleOldTestamentPage from '@page/bible/BibleOldTestamentPage.tsx'
+import BibleNewTestamentPage from '@page/bible/BibleNewTestamentPage.tsx'
+import PBSPage from '@page/pbs/PBSPage.tsx'
+import PBSCreatePage from '@page/pbs/PBSCreatePage.tsx'
+import PBSReadPage from '@page/pbs/PBSReadPage.tsx'
+import QTPage from '@page/qt/QTPage.tsx'
+import QTCreatePage from '@page/qt/QTCreatePage.tsx'
+import QTReadPage from '@page/qt/QTReadPage.tsx'
+/** Style */
+import GlobalStyle from '@style/common/GlobalStyle.ts'
 
 const App: React.FC = () => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle/>
+
+      <BrowserRouter>
       <Header/>
 
       <SideBar/>
 
       <Routes>
         <Route path={"/"} element={<HomePage/>}/>
-        <Route path={"/bible"} element={<BiblePage/>}/>
-        <Route path={"/pbs"} element={<PBSPage/>}/>
-        <Route path={"/pbsCreate"} element={<PBSCreatePage/>}/>
-        <Route path={"/qt"} element={<QTPage/>}/>
-        <Route path={"/qtCreate"} element={<QTCreatePage/>}/>
-      </Routes>
+        <Route path={"/oldTestament"} element={<BibleOldTestamentPage/>}/>
+        <Route path={"/newTestament"} element={<BibleNewTestamentPage/>}/>
 
+        <Route path={"/pbs"} element={<PBSPage/>}/>
+        <Route path={"/pbs/create"} element={<PBSCreatePage/>}/>
+        <Route path={"/pbs/read/:id"} element={<PBSReadPage/>}/>
+
+        <Route path={"/qt"} element={<QTPage/>}/>
+        <Route path={"/qt/create"} element={<QTCreatePage/>}/>
+        <Route path={"/qt/read/:id"} element={<QTReadPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 /** React */
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
 /** Component */
 import App from '@/App.tsx'
 /** Style */
-import GlobalStyle from '@style/common/GlobalStyle.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GlobalStyle/>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
 )
