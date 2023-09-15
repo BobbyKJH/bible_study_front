529a94da956e5055e619b1f6bc25a4a7dbc7ec63
bobby <rlatnguscjsw@gmail.com>
2023-09-15 10:54:54 +0900

    Feat : dateFormat prop
    - dateFormat prop 생성

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,10 +1,8 @@
-
-const DateFormat = (): string => {
-	const fullDate = new Date();
-
-	let year = fullDate.getFullYear();
-	let month: number | string = fullDate.getMonth() + 1;
-	let day: number | string = fullDate.getDate();
+/** Date YYYY-MM-DD */
+const DateFormat = (date: Date): string => {
+	let year = date.getFullYear();
+	let month: number | string = date.getMonth() + 1;
+	let day: number | string = date.getDate();
 
 	month = month >= 10 ? month : '0' + month;
 	day = day >= 10 ? day : '0' + day;
