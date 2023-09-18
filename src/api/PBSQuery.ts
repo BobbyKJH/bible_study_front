359b02553189592431421a8216ff0e29f1e9b387
bobby <rlatnguscjsw@gmail.com>
2023-09-19 00:05:33 +0900

    Feat : pbs CRUD
    - pbs CRUD useQuery, useMutation 생성

--- src/api/PBSQuery.ts
+++ src/api/PBSQuery.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 /** PBS 게시판 */
 const getPBSList = async (page: number) => {
 	try{
-		const res = await axios.get(`http://localhost:8083/pbs/page?page=${page}`)
+		const res = await axios.get(`http://localhost:8083/pbs?page=${page}`)
 		return res.data
 	} catch (err) {
 		console.error(err)
@@ -19,25 +19,42 @@ const getPBSDetailList = async (page:  string) => {
 		const res = await axios.get(`http://localhost:8083/pbs/${page}`)
 		return res.data
 	} catch (err) {
-		console.error(err)
+		throw err;
 	}
 }
 
 export const useDetailQuery = (id: string) => useQuery(["PBSDetail"], () => getPBSDetailList(id as string))
 
-/** Create PBS Mutate */
+/** PBS 생성 */
 const createPBSList = async (data: any) => {
 	try{
 		const res = await axios.post("http://localhost:8083/pbs", data)
 		console.log(res.data)
 		return res.data
 	} catch (err) {
-		console.error(err)
+		throw err
 	}
 }
 
 export const useCreateMutation = () => useMutation(createPBSList, {
-	onError: () => alert("작성 실패"),
-	onSuccess: () => alert("작성 성공")
+	onSuccess: () => {
+		alert( "작성 완료" )
+	},
+	onError: () => {
+		alert( "작성 실패" )
+	}
 });
+
+/** PBS 삭제 */
+const deletePBS = async (id: number) => {
+	try{
+		const res = await axios.delete(`http://localhost:8083/pbs/${id}`)
+		console.log(res.data)
+		return res.data
+	} catch (err) {
+		throw err
+	}
+}
+
+export const useDeleteMutation = () => useMutation(deletePBS);
 
