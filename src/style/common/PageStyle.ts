23749de87aea5a2d89f49ca78886e2cdf3e9ad4b
bobby <rlatnguscjsw@gmail.com>
2023-09-27 00:10:02 +0900

    Style : PageStyle
    - Common Style Page 생성

--- src/style/common/PageStyle.ts
+++ src/style/common/PageStyle.ts
@@ -1,8 +1,25 @@
 import styles from "styled-components"
 
 export const PageContainer = styles.div`
-		margin-left: 200px;
-		@media (max-width: 900px) {
-		  margin-left: 0
-		}
+	margin-left: 200px;
+	@media (max-width: 900px) {
+	  margin-left: 0
+	}
 	`
+
+export const NoticeContainer = styles.div`
+	display: flex;
+	flex-direction: column;
+	height: 500px;
+	border:1px solid black;
+	justify-content: space-between;
+`
+
+export const MyPageNoticeContainer = styles.div`
+	display: flex;
+	flex-direction: column;
+	height: 500px;
+	width: 250px;
+	border:1px solid black;
+	justify-content: space-between;
+`
