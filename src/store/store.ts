bb58a2bab76a0af3d2aed2e145e14fc3ca9ff7b7
bobby <rlatnguscjsw@gmail.com>
2023-09-27 00:06:22 +0900

    Feat : 임시 저장 기능 생성
    - PBS 임시 저장 기능 생성

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import DateFormat from '@utils/dateFormat.ts'
 
 interface UseHeader {
 	sideBar: boolean;
@@ -12,3 +14,51 @@ export const useSideBar = create<UseHeader>((set) => ({
 	handleOpenSideBar: () => set({ sideBar: true }),
 	handleCloseSideBar: () => set({ sideBar: false }),
 }));
+
+interface PBSCreateProps {
+	pbs: {
+		book:        string;
+		chapter:     number | null;
+		startVerse:  number | null;
+		endVerse:    number | null;
+		date:        string;
+		content:     string;
+		showData:    string;
+	},
+	savePBS: (object: any) => void,
+	clearPBS: any
+}
+
+/** PBS 생성 */
+export const usePBSCreate = create<PBSCreateProps>()(
+	persist(
+		(set) => ({
+			pbs: {
+				book:       "",
+				chapter:    null,
+				startVerse: null,
+				endVerse:   null,
+				date:       DateFormat(new Date()),
+				content:    "",
+				showData:   "Y"
+			},
+
+			savePBS: (object) => set(() => ({ pbs: object })),
+
+			clearPBS: () => set((): any => ({ pbs: {
+					book:       "",
+					chapter:    "",
+					startVerse: "",
+					endVerse:   "",
+					date:       DateFormat(new Date()),
+					content:    "",
+					showData:   "Y"
+				} }))
+		}),
+
+		{
+			name: 'pbs-create',
+			// storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+		}
+	)
+)
