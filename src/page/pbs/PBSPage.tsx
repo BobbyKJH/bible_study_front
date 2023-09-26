84a57828f046695c90aa999340ab3683e341a6ed
bobby <rlatnguscjsw@gmail.com>
2023-09-27 00:08:49 +0900

    Feat : 게시판
    - PBS 게시판 기능 생성
    - QT 게시판 기능 생성

--- src/page/pbs/PBSPage.tsx
+++ src/page/pbs/PBSPage.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import { PageContainer } from '@style/common/PageStyle.ts'
+/** React */
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom'
+/** Custom Hook */
+import usePage from '@/hook/usePage.ts'
+/** Utils */
+import pageIndex from '@utils/pageIndex.ts'
+import pageCount from '@utils/pageCount.ts'
+/** Api */
+import { usePBSQuery } from '@/api/PBSQuery.ts'
+/** Type */
+import Bible from '@type/Bible'
+/** Style */
+import { NoticeContainer, PageContainer } from '@style/common/PageStyle.ts'
+import { Container, Pagination } from '@mui/material'
+
 
 const PBSPage: React.FC = () => {
+	/** Page */
+	const { page, handleClickPage  } = usePage();
+	/** PBS 게시판 */
+	const { data, isLoading, refetch } = usePBSQuery(page);
+
+	useEffect(() => {
+		refetch();
+	}, [page])
+
 	return (
 		<PageContainer>
-			PBS
+			<Container maxWidth="lg" sx={{ border: "1px solid black", marginBottom: "20px", height:"500px" ,display:"flex", flexDirection:"column",
+			justifyContent:"space-between"
+			}}>
+
+				<div>
+					{!isLoading ?
+						<NoticeContainer>
+							<div>
+								{data.pbs.map((item: Bible.Notice, index: number) => (
+									<Link to={`/pbs/read/${item.id}`} key={item.id}>
+										<div>
+											<span>{pageIndex(index, page)}</span>
+											<span>{item.book}</span>
+											<span>{item.chapter}장</span>
+											<span>{item.startVerse}절</span>
+											<span>{item.endVerse}절</span>
+											<span>{item.id}</span>
+										</div>
+									</Link>
+								))}
+							</div>
+
+							<Pagination
+								count={pageCount(data.length)}
+								page={page}
+								onChange={handleClickPage}
+								showFirstButton
+								showLastButton
+								sx={{ display:"flex",justifyContent:"center" }}
+							/>
+
+						</NoticeContainer>
+						:
+						null
+					}
+				</div>
+
+
+					<Link to={"/pbs/create"}>글쓰기</Link>
+			</Container>
 		</PageContainer>
 	);
 };
--- src/page/qt/QTPage.tsx
+++ src/page/qt/QTPage.tsx
@@ -1,10 +1,61 @@
+/** React */
 import React from 'react';
-import { PageContainer } from '@style/common/PageStyle.ts'
+import { Link } from 'react-router-dom'
+/** Query */
+import { useQTQuery } from '@/api/QTQuery.ts'
+/** Hook */
+import usePage from '@/hook/usePage.ts'
+/** Utils */
+import pageIndex from '@utils/pageIndex.ts'
+import pageCount from '@utils/pageCount.ts'
+/** Type */
+import Bible from '@type/Bible'
+/** Style */
+import { Pagination } from '@mui/material'
+import { NoticeContainer, PageContainer } from '@style/common/PageStyle.ts'
+/** Icon */
+import { IoCreateOutline } from "react-icons/io5";
 
 const QTPage: React.FC = () => {
+	const { page, handleClickPage } = usePage();
+	const { data, isLoading } = useQTQuery(page);
+
+	console.log(!isLoading && data)
+
 	return (
 		<PageContainer>
-			QT
+			<IoCreateOutline/>
+			<div>
+				{!isLoading ?
+					<NoticeContainer>
+						<div>
+							{data.qt.map((item: Bible.Notice, index: number) => (
+								<Link to={`/qt/read/${item.id}`} key={item.id}>
+									<div>
+										<span>{pageIndex(index, page)}</span>
+										<span>{item.book}</span>
+										<span>{item.chapter}장</span>
+										<span>{item.startVerse}절</span>
+										<span>{item.endVerse}절</span>
+										<span>{item.id}</span>
+									</div>
+								</Link>
+							))}
+						</div>
+
+						<Pagination
+							count={pageCount(data.length)}
+							page={page}
+							onChange={handleClickPage}
+							showFirstButton
+							showLastButton
+							sx={{ display:"flex",justifyContent:"center" }}
+						/>
+					</NoticeContainer>
+					:
+					null
+				}
+			</div>
 		</PageContainer>
 	);
 };
