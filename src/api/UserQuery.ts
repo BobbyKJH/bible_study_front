a8bec9891db8e32110ce069ee2e69bb7cbe0dab1
bobby <rlatnguscjsw@gmail.com>
2023-10-06 01:07:27 +0900

    Feat : UserId Sha256
    - Session Storage sha256 방식으로 저장

--- src/api/UserQuery.ts
+++ src/api/UserQuery.ts
@@ -1,13 +1,15 @@
 import axios from 'axios'
 import { useMutation } from '@tanstack/react-query'
+/** Sha256 */
+import { sha256 } from 'js-sha256'
 
 /** User 조회 */
 const getUserId = async (data: { userId: string }) =>{
 	try{
 		const res = await axios.post(`http://localhost:8083/user/userId`, data);
-		sessionStorage.setItem("userId", res.data.userId);
+		sessionStorage.setItem("userId", sha256(res.data.userId));
 		sessionStorage.setItem("userName", res.data.userName);
-		sessionStorage.setItem("userAuth", res.data.userAuth);
+		sessionStorage.setItem("userAuth", sha256(res.data.userAuth));
 		
 		return res.data
 	} catch (err) {
