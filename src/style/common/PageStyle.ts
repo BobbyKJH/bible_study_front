4e6daaba1ff791d8f2a1be74042237009180f224
bobby <rlatnguscjsw@gmail.com>
2023-10-06 01:10:03 +0900

    Style : Page Style
    - Styled comnponent

--- src/style/common/PageStyle.ts
+++ src/style/common/PageStyle.ts
@@ -1,21 +1,20 @@
-import styles from "styled-components"
+import styled from "styled-components"
 
-export const PageContainer = styles.div`
+export const PageContainer = styled.div`
 	margin-left: 200px;
 	@media (max-width: 900px) {
 	  margin-left: 0
 	}
 	`
 
-export const NoticeContainer = styles.div`
+export const NoticeContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	height: 500px;
-	border:1px solid black;
 	justify-content: space-between;
 `
 
-export const MyPageNoticeContainer = styles.div`
+export const MyPageNoticeContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	height: 500px;
