57d93cafb56df2394108075d3bd5605a0ee007a2
bobby <rlatnguscjsw@gmail.com>
2023-10-06 01:11:29 +0900

    Feat : Chart 주간
    - 주간 Chart Home Page에 생성

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+/** React */
+import PBSChart from '@components/home/PBSChart.tsx'
+import QTChart from '@components/home/QTChart.tsx'
+/** Query */
+import { useAdminQuery } from '@/api/AdminQuery.ts'
+/** Style */
 import { PageContainer } from '@style/common/PageStyle.ts'
 
 
+
 const HomePage: React.FC = () => {
+    const { data, isLoading } = useAdminQuery();
 
     return(
-      <PageContainer>Home</PageContainer>
-
+        <PageContainer>
+            {
+                !isLoading ?
+                        <div>
+                            <PBSChart startDate={data.startDate} endDate={data.endDate}/>
+                            <QTChart startDate={data.startDate} endDate={data.endDate}/>
+                            1
+                        </div>
+                    :
+                    null
+            }
+        </PageContainer>
     )
 };
 
