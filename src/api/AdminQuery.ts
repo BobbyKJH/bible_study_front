61eace32003a79f5c160c1fdcf9a4592622fb231
bobby <rlatnguscjsw@gmail.com>
2023-10-06 01:08:27 +0900

    Feat : 주간 QT, PBS Chart Query
    - 주간 Chart Query 생성

--- src/api/AdminQuery.ts
+++ src/api/AdminQuery.ts
@@ -1,17 +1,46 @@
 import axios from 'axios'
 import { useMutation, useQuery } from '@tanstack/react-query'
 
-/** 주간 PBS */
+
+
 const getAdminList = async () => {
 	try{
 		const res = await axios.get("http://localhost:8083/admin")
 		return res.data
 	} catch (err) {
-		console.error(err)
+		throw err;
 	}
 }
 
 export const useAdminQuery = () => useQuery(["admin"], () => getAdminList());
+
+/** PBS Chart */
+const getPBSChart = async (startDate: string, endDate: string) => {
+	try {
+		const res = await axios.get(`http://localhost:8083/analytics/pbs?startDate=${startDate}&endDate=${endDate}`);
+		return res.data;
+	} catch (err) {
+		throw err;
+	}
+}
+
+/** 주간 PBS Chart */
+export const usePBSChartQuery = (startDate: string, endDate: string) => useQuery(["PBSChart"], () => getPBSChart(startDate, endDate));
+
+/** QT Chart */
+const getQTChart = async (startDate: string, endDate: string) => {
+	try {
+		const res = await axios.get(`http://localhost:8083/analytics/qt?startDate=${startDate}&endDate=${endDate}`);
+		return res.data;
+	} catch (err) {
+		throw err;
+	}
+}
+
+/** 주간 QT Chart */
+export const useQTChartQuery = (startDate: string, endDate: string) => useQuery(["QTChart"], () => getQTChart(startDate, endDate));
+
+
 
 /** 주간 PBS 업데이트 */
 const editAdmin = async (data: any) => {
