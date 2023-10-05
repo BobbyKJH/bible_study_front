8dd4460788feb30cdc756b1160eb4cdc3e3306d3
bobby <rlatnguscjsw@gmail.com>
2023-10-06 01:41:20 +0900

    Feat : 게시판 페이지 개수 설정
    - 나머지가 0일 경우 1을 더하지 않고 그 값만 계산해서 출력 한다

--- src/utils/pageCount.ts
+++ src/utils/pageCount.ts
@@ -1,6 +1,7 @@
 /** 게시판 페이지 개수 설정 */
 const pageCount = (num: number): number => {
-	const pageNum: number = Math.floor(num / 10) + 1;
+	const pageMath = Math.floor(num / 10);
+	const pageNum: number = num % 10 === 0 ? pageMath : pageMath + 1;
 
 	const page: string = pageNum.toFixed(0);
 
