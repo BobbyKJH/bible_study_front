655d6c6a4b2cd052f50c840e588743c0eca7016c
bobby <rlatnguscjsw@gmail.com>
2023-10-11 04:39:07 +0900

    Feat : pageCount
    - page 검색 기능 추가로 인한 utils 함수 생성

--- src/utils/pageCount.ts
+++ src/utils/pageCount.ts
@@ -1,11 +1,11 @@
+import Bible from '@type/Bible'
+
 /** 게시판 페이지 개수 설정 */
-const pageCount = (num: number): number => {
-	const pageMath = Math.floor(num / 10);
-	const pageNum: number = num % 10 === 0 ? pageMath : pageMath + 1;
+const pageCount = (notice: Bible.Notice[], search: string): number => {
+	const page = notice.filter((item) => item.book.includes(search));
+	const pageCount = page.length / 10;
 
-	const page: string = pageNum.toFixed(0);
-
-	return Number(page);
+	return Math.ceil(pageCount);
 };
 
 export default pageCount;
