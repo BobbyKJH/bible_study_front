8fc2bf94d3d2b6a917f1bda2021f1480b0c5012e
bobby <rlatnguscjsw@gmail.com>
2023-10-13 00:34:33 +0900

    Style : FooterStyle
    - FooterStyle Position Fixed height

--- src/style/common/FooterStyle.ts
+++ src/style/common/FooterStyle.ts
@@ -2,20 +2,24 @@ import styled from "styled-components";
 
 /** 관리자 권한 Footer */
 export const FooterContainer = styled.div<{ content: string }>`
-  display: flex;
-	justify-content: ${props => props.content};
-	align-items: center;
-	background-color: #fff;
-	box-sizing: border-box;
-	position: fixed;
-	bottom: 0;
-	left: 0;
-	right: 0;
 	height: 70px;
-	padding: 0 30px;
-  margin-left: 200px;
-	box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
-  @media (max-width: 900px) {
-    margin-left: 0
-  }
+	div{
+    display: flex;
+    justify-content: ${props => props.content};
+    align-items: center;
+    background-color: #fff;
+    box-sizing: border-box;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 70px;
+    padding: 0 30px;
+    margin-left: 200px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+    @media (max-width: 900px) {
+      margin-left: 0
+    }
+	}
+  
 `
