caf3e8238890e79e97cae4c20ff911bb6d817d5a
bobby <rlatnguscjsw@gmail.com>
2023-10-14 00:10:47 +0900

    Feat : Utils Type
    - 공통으로 사용하는 type 추가

--- src/type/Bible.d.ts
+++ src/type/Bible.d.ts
@@ -1,5 +1,7 @@
+import { UseFormRegister } from 'react-hook-form'
 
 declare namespace Bible{
+
 	interface Notice{
 		id:          number;
 		book:        string;
@@ -9,15 +11,43 @@ declare namespace Bible{
 		content:     string;
 		createAt:    string;
 		updateAt:    string;
-		data:        string;
+		date:        string;
 		userId:      string;
 		userName:    string;
 		showData:    string;
 	}
 
 	interface Create{
-
+		date: string,
+		book:       string,
+		chapter:    string | null,
+		startVerse: string | null,
+		endVerse:   string | null,
+		content:    string,
+		showData:   string
 	}
+
+	interface Read {
+		value: string | number;
+		verse?: string;
+		books?: {
+			id: number;
+			book: string;
+		}
+	}
+
+	interface Text {
+		register: UseFormRegister<any>;
+		name:     string;
+		verse?:   string;
+		label?:   string;
+	}
+
+	interface User{
+		userId: string;
+		userName: string;
+	}
+
 }
 
 export default Bible;
