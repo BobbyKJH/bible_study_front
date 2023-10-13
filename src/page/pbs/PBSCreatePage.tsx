54030f7f231112295da93c516a69a41f6b1c44e3
bobby <rlatnguscjsw@gmail.com>
2023-10-14 00:12:01 +0900

    Feat : QT PBS Create Page
    - 전역관리 변경으로 인한 Create Page 임시 저장 기능 수정

--- src/page/pbs/PBSCreatePage.tsx
+++ src/page/pbs/PBSCreatePage.tsx
@@ -1,41 +1,42 @@
 /** React */
 import React from 'react';
 import { useNavigate } from 'react-router'
+/** Hook */
 import { useForm } from 'react-hook-form'
+/** Recoil */
+import { PBSNoticeAtom } from '@/store/noticeAtom.ts'
+import { useRecoilState, useResetRecoilState } from 'recoil'
 /** Api */
 import { useCreatePBSMutation } from '@/api/PBSQuery.ts'
-/** Zustand */
-import { usePBSCreate } from '@/store/store.ts'
 /** Component */
 import TextSelect from '@components/create/TextSelect.tsx'
 import TextNumber from '@components/create/TextNumber.tsx'
 import TextMultiField from '@components/create/TextMultiField.tsx'
 /** Utils */
 import { newTestament, oldTestament } from '@utils/arr/BibleBooks.ts'
-/** Style */
-import { PageContainer } from '@style/common/PageStyle.ts'
-import { Paper } from '@mui/material'
 /** Type */
 import Bible from '@type/Bible'
-import DateFormat from '@utils/dateFormat.ts'
+/** Style */
+import { Button } from '@mui/material'
+import { NoticeForm } from '@style/notice/NoticeStyle.ts'
+import { PageContainer } from '@style/common/PageStyle.ts'
+import { FooterContainer } from '@style/common/FooterStyle.ts'
 
 
 const PBSCreatePage: React.FC = () => {
-	const navigate = useNavigate()
-
-	const { pbs, savePBS, clearPBS } = usePBSCreate();
-
-	const { mutate } = useCreatePBSMutation();
+	const [notice, setNotice] = useRecoilState<Bible.Create>(PBSNoticeAtom);
+	const resetNotice = useResetRecoilState(PBSNoticeAtom);
 
 	const { register, handleSubmit, watch } = useForm({
 		mode: "onChange",
 		defaultValues: {
-			date: DateFormat(new Date()),
-			...pbs,
+			...notice,
 			userId: sessionStorage.getItem("userId"),
 			userName: sessionStorage.getItem("userName")},
 	});
+	const { mutate } = useCreatePBSMutation();
 
+	const navigate = useNavigate();
 
 	/** Post */
 	const createPBS = (data: Bible.Create): void => {
@@ -43,39 +44,64 @@ const PBSCreatePage: React.FC = () => {
 			mutate(data,
 				{ onSuccess: () => navigate("/pbs") }
 			);
-			clearPBS();
+			resetNotice();
 		}
 	}
 
 	/** 암시 저장 */
 	const tmpSave = (event: React.MouseEvent<HTMLButtonElement>): void => {
-		event.preventDefault()
+		event.preventDefault();
 		if(window.confirm("임시 저장 하시겠습니까?")){
-			alert("임시저장 되었습니다.")
-			savePBS(watch())
+			alert("임시저장 되었습니다.");
+			setNotice(watch());
 		}
-	}
+	};
+
+	/** 뒤로가기 */
+	const handleBackPage = (): void => {
+		navigate("/pbs")
+	};
 
 	return (
 		<PageContainer>
-			<Paper>
-				<form onSubmit={handleSubmit(createPBS)}>
+			<NoticeForm onSubmit={handleSubmit(createPBS)}>
 					<TextSelect register={register} name={"book"} oldTestament={oldTestament} newTestament={newTestament} watch={watch}/>
 
 					<TextNumber register={register} name={"chapter"} verse={"장"}/>
+
 					<div>
 						<TextNumber register={register} name={"startVerse"} verse={"절"}/>
 						<TextNumber register={register} name={"endVerse"} verse={"절"}/>
 					</div>
-					<input type={"date"} {...register("date")}/>
 
-					<TextMultiField register={register} name={"content"} />
+				<input type={"date"} {...register("date")}/>
 
-					<button type={"submit"}>생성</button>
-					<button onClick={tmpSave}>임시저장</button>
-				</form>
-			</Paper>
+				<hr/>
+
+				<TextMultiField register={register} name={"content"} />
+			</NoticeForm>
+
+			<FooterContainer content={"space-between"}>
+				<div>
+					<Button variant="outlined" onClick={handleBackPage}>나가기</Button>
+
+					<span>
+						<Button
+							variant="contained"
+							onClick={handleSubmit(createPBS)}
+							sx={{ margin: "0 20px"}}
+						>생성
+						</Button>
+
+						<Button variant="outlined" onClick={tmpSave}>
+							임시 저장
+						</Button>
+					</span>
+				</div>
+			</FooterContainer>
 		</PageContainer>
+
+
 	);
 };
 
--- src/page/qt/QTCreatePage.tsx
+++ src/page/qt/QTCreatePage.tsx
@@ -1,12 +1,106 @@
+/** React */
 import React from 'react';
+import { useNavigate } from 'react-router'
+import { useForm } from 'react-hook-form'
+/** Api */
+import { useCreateQTMutation } from '@/api/QTQuery.ts'
+/** Utils */
+import { newTestament, oldTestament } from '@utils/arr/BibleBooks.ts'
+/** Component */
+import TextSelect from '@components/create/TextSelect.tsx'
+import TextNumber from '@components/create/TextNumber.tsx'
+import TextMultiField from '@components/create/TextMultiField.tsx'
+/** Type */
+import Bible from '@type/Bible'
+/** Style */
+import { Button } from '@mui/material'
+import { NoticeForm } from '@style/notice/NoticeStyle.ts'
 import { PageContainer } from '@style/common/PageStyle.ts'
+import { FooterContainer } from '@style/common/FooterStyle.ts'
+import { useRecoilState, useResetRecoilState } from 'recoil'
+import { QTNoticeAtom } from '@/store/noticeAtom.ts'
 
 const QTCreatePage: React.FC = () => {
+	const [notice, setNotice] = useRecoilState<Bible.Create>(QTNoticeAtom);
+	const resetNotice = useResetRecoilState(QTNoticeAtom);
+
+
+	const navigate = useNavigate()
+
+	const { mutate } = useCreateQTMutation();
+
+	const { register, handleSubmit, watch } = useForm({
+		mode: "onChange",
+		defaultValues: {
+			...notice,
+			userId: sessionStorage.getItem("userId"),
+			userName: sessionStorage.getItem("userName")},
+	});
+
+	/** Post */
+	const createQT = (data: Bible.Create): void => {
+		if(window.confirm("QT 작성하시겠습니까?")){
+			mutate(data,
+				{ onSuccess: () => navigate("/qt") }
+			);
+			resetNotice();
+		}
+	}
+
+	const tmpSave = (event: React.MouseEvent<HTMLButtonElement>): void => {
+		event.preventDefault();
+		if(window.confirm("임시 저장 하시겠습니까?")){
+			alert("임시저장 되었습니다.");
+			setNotice(watch());
+		}
+	};
+
+	const handleBackPage = (): void => {
+		navigate("/qt");
+	};
+
 	return (
 		<PageContainer>
+				<NoticeForm onSubmit={handleSubmit(createQT)}>
+					<div style={{display: "flex"}}>
+						<TextSelect register={register} name={"book"} oldTestament={oldTestament} newTestament={newTestament} watch={watch}/>
 
+						<TextNumber register={register} name={"chapter"} verse={"장"}/>
+
+						<div>
+							<TextNumber register={register} name={"startVerse"} verse={"절"}/>
+							<TextNumber register={register} name={"endVerse"} verse={"절"}/>
+						</div>
+					</div>
+
+					<input type={"date"} {...register("date")}/>
+
+					<hr/>
+
+					<TextMultiField register={register} name={"content"} />
+
+				</NoticeForm>
+
+			<FooterContainer content={"space-between"}>
+				<div>
+					<Button variant="outlined" onClick={handleBackPage}>나가기</Button>
+
+					<span>
+						<Button
+							variant="contained"
+							onClick={handleSubmit(createQT)}
+							sx={{ margin: "0 20px"}}
+						>생성
+						</Button>
+
+						<Button variant="outlined" onClick={tmpSave}>
+							임시 저장
+						</Button>
+					</span>
+				</div>
+			</FooterContainer>
 		</PageContainer>
 	);
 };
 
-export default QTCreatePage;
+export default QTCreatePage;222
