c5351e0567c7bf296f9142d6ea078478b8f6fc97
bobby <rlatnguscjsw@gmail.com>
2023-10-15 00:10:17 +0900

    Style : PageStyle
    - PageContainer height 수정

--- src/style/common/PageStyle.ts
+++ src/style/common/PageStyle.ts
@@ -1,17 +1,21 @@
 import styled from "styled-components"
 
 export const PageContainer = styled.div`
-	margin-left: 200px;
-	@media (max-width: 900px) {
-	  margin-left: 0
-	}
+  margin-left: 200px;
+	height: calc(100vh - 64px);
+	box-sizing: border-box;
+  @media (max-width: 900px) {
+    margin-left: 0
+  }
 	`
 
-export const NoticeContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	height: 500px;
-	justify-content: space-between;
+export const NoticeCreateContainer = styled.div`
+	width: 90%;
+	box-sizing: border-box;
+  margin-left: 200px;
+  @media (max-width: 900px) {
+    margin-left: 0
+  }
 `
 
 export const MyPageNoticeContainer = styled.div`
