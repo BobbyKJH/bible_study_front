bd8d09d1e9ff4ecadfe75b7c672690bc7bb95069
bobby <rlatnguscjsw@gmail.com>
2023-10-19 00:06:50 +0900

    Style : NoticeStyle
    - Notice Style

--- src/style/notice/NoticeStyle.ts
+++ src/style/notice/NoticeStyle.ts
@@ -2,24 +2,23 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom';
 import { Paper } from '@mui/material';
 
-
-
 export const NoticePaper = styled(Paper)`
 	position: relative;
 	display: flex;
 	flex-direction: column;
 	margin: 0 auto;
 	justify-content: space-between;
-	height: 500px;
 	width: 100%;
 	padding: 0;
 `;
+
 /** 게시판 이름 */
 export const NoticeTitle = styled.span`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
 	padding: 10px 10px;
+	cursor: pointer;
 	p {
     font-size: 2rem;
     font-weight: 900;
@@ -32,25 +31,52 @@ export const NoticeTitle = styled.span`
 `;
 
 /** 게시판 */
-export const NoticeItem = styled(Link)`
+export const NoticeItemContainer = styled.div`
 	display: flex;
   padding: 10px;
 	box-sizing: border-box;
-	border: 1px solid black;
+	border-bottom: 2px solid #808080;
 	&:hover {
 		text-decoration: underline;
+		background-color: #80808070;
 	}
 `;
 /** 게시판 순서 */
 export const NoticeNum = styled.span`
-	width: 20%;
+	width: 10%;
 	text-align: center;
 `;
+
 /** 게시판 내용 */
-export const NoticeContent = styled.span``;
+export const NoticeContent = styled.span`
+  width: 20%;
+`;
+
 /** 게시판 성경 */
-export const NoticeBook = styled.span``;
+export const NoticeBook = styled.span`
+  width: 20%;
+`;
+export const NoticeChapter = styled.span`
+  width: 20%;
+`;
+
 /** 게시판 절 */
-export const NoticeVerse = styled.span``;
+export const NoticeVerse = styled.span`
+  width: 20%;
+`;
+
 /** 게시판 날짜 */
-export const NoticeDate = styled.span``;
+export const NoticeDate = styled.span`
+  width: 20%;
+`;
+
+export const NoticeForm = styled.form`
+	width: 95%;
+	margin: 0 auto;
+`;
+
+export const NoticeCreateBtn = styled(Link)`
+	position: absolute;
+	right: 40px;
+	padding: 10px;
+`;
