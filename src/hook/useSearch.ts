8f2c865a30fef01e07dbe0775e7a1fd4b8611d15
bobby <rlatnguscjsw@gmail.com>
2023-10-18 17:15:12 +0900

    Feat : useSearch
    - 검색기능 수정으로 인한 Custom Hook 수정

--- src/hook/useSearch.ts
+++ src/hook/useSearch.ts
@@ -1,12 +1,18 @@
-import { useCallback, useState } from 'react';
+/** React */
+import { useCallback } from 'react';
+/** Atom */
+import { RecoilState, useRecoilState, useResetRecoilState } from 'recoil'
+/** Type */
 import { SelectChangeEvent } from '@mui/material'
 
-const useSearch = () => {
-	const [search, useSearch] = useState<string>("")
+const useSearch = (initialAtom: RecoilState<string>, resetAtom: RecoilState<number>) => {
+	const [search, useSearch] = useRecoilState<string>(initialAtom);
+	const reset = useResetRecoilState(resetAtom);
 
 	const handleChangeSearch = useCallback((event: SelectChangeEvent): void => {
 		const { value } = event.target;
 		useSearch(value);
+		reset();
 	}, [search]);
 
 	return { search, handleChangeSearch };
