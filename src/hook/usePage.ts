951d02fac1f46976abcfe6b90d02646722592516
bobby <rlatnguscjsw@gmail.com>
2023-10-18 17:14:51 +0900

    Feat : usePage
    - 게시판 및 마이페이지 게시판 을 위한 Custom Hook 수정

--- src/hook/usePage.ts
+++ src/hook/usePage.ts
@@ -1,15 +1,18 @@
 /** React */
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
+/** Atom */
+import { RecoilState, useRecoilState, useResetRecoilState } from 'recoil';
 
-const usePage = () => {
-	const [page, setPage] = useState<number>(1);
-
+const usePage = (initailAtom: RecoilState<number>) => {
+	const [page, setPage] = useRecoilState(initailAtom);
+	const resetPage = useResetRecoilState(initailAtom);
+	
 	const handleClickPage = useCallback((event: React.ChangeEvent<unknown>, value: number): void => {
 		window.scrollTo(0, 0);
 		setPage(value)
 	}, [page]);
 
-	return { page, setPage, handleClickPage };
+	return { page, setPage, handleClickPage, resetPage };
 };
 
 export default usePage;
