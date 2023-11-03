8daddf3654975d1cc4c3770017a4ad3d2a170d03
bobby <rlatnguscjsw@gmail.com>
2023-11-04 00:04:34 +0900

    Feat : pageIndex Type
    - page type 변경

--- src/utils/pageIndex.ts
+++ src/utils/pageIndex.ts
@@ -1,6 +1,6 @@
 /** 게시판 index */
-const pageIndex = (index: number, page: number): number => {
-	return (index + 1) + ((page - 1) * 10);
+const pageIndex = (index: number, page: number | string | undefined): number => {
+	return (index + 1) + ((Number(page) - 1) * 10);
 };
 
 export default pageIndex;
