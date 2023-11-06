68b43725281f5467cd01286d0a04d4117eff7f7f
bobby <rlatnguscjsw@gmail.com>
2023-11-06 23:24:21 +0900

    Feat : LoginPage
    - LoginPage
    - Root Layout

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<>
+			<h1>ss</h1>
+			{children}
+		</>
+	)
+}
+
+export default Layout;
