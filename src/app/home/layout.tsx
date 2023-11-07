1aaf8d9bc0c30033445d9bf55a169b558683432a
bobby <rlatnguscjsw@gmail.com>
2023-11-08 00:56:42 +0900

    Feat : Layout
    - 공통 Header, Sidebar 적용
    - Layout 반응형 Style

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import Header from '@/components/common/header/Header';
+import Sidebar from '@/components/common/sidebar/Sidebar';
+
+import ScrollToTop from '@/components/common/ScrollToTop';
+
+import styles from "@/app/home/layout.module.css"
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
+
 	return (
 		<>
-			<h1>ss</h1>
-			{children}
+			<ScrollToTop/>
+
+			<Header/>
+
+			<Sidebar/>
+
+			<div className={styles.container}>
+				{children}
+			</div>
 		</>
 	)
 }
