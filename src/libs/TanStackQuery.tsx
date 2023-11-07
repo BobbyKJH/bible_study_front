3bde293e371ae37f14df87960ca1e4f78b638eb8
bobby <rlatnguscjsw@gmail.com>
2023-11-07 23:30:45 +0900

    Feat : RecoilRoot
    - TanStackQuery RecoilRoot Add

--- src/libs/TanStackQuery.tsx
+++ src/libs/TanStackQuery.tsx
@@ -1,14 +1,19 @@
 "use client"
 import React, { useState } from 'react';
+/** Provider */
+import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 
 const TanStackQuery = ({ children }: { children: React.ReactNode }) => {
 	const [queryClient] = useState(() => new QueryClient());
+	
 	return (
-		<QueryClientProvider client={queryClient}>
-			{children}
-		</QueryClientProvider>
+		<RecoilRoot>
+			<QueryClientProvider client={queryClient}>
+				{children}
+			</QueryClientProvider>
+		</RecoilRoot>
 	);
 };
 
