3a33a81d6338d75fd185771ad4d3185316ced75e
bobby <rlatnguscjsw@gmail.com>
2023-11-10 00:41:22 +0900

    Feat : App
    - Page Route
    - Recoil Connect
    - React-Query Connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import AuthRoutes from '@page/common/AuthRoutes.tsx';
-import HomePage from '@page/login/HomePage.tsx';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+/** Query-Client */
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+/** Atom */
+import { RecoilRoot } from 'recoil';
+/** Page */
+import HomePage from '@page/HomePage.tsx';
+import LoginPage from '@page/login/LoginPage.tsx';
+import AuthRoutes from '@page/common/AuthRoutes.tsx';
 
 const App: React.FC = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AuthRoutes/>}>
-          <Route path={"/home"} element={<HomePage/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<LoginPage/>} />
+
+            <Route element={<AuthRoutes/>}>
+              <Route path={"/home"} element={<HomePage/>} />
+            </Route>
+
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </RecoilRoot>
   )
 }
 
