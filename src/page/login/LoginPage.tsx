a33fc76e84568f1b026e19d67d2ff9c02ceb9ddc
bobby <rlatnguscjsw@gmail.com>
2023-11-10 01:01:30 +0900

    Feat : LoginPage
    - Login 기능 개발

--- src/page/login/LoginPage.tsx
+++ src/page/login/LoginPage.tsx
@@ -1,10 +1,32 @@
+import { useUserMutation } from '@/api/UserQuery.ts';
+import { setCookie } from '@/libs/cookie.ts';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
 
 const LoginPage: React.FC = () => {
+	const navigate = useNavigate();
+	const { mutate } = useUserMutation();
+	const { register, handleSubmit} = useForm({
+		defaultValues: {
+			"userId": ""
+		}
+	})
+
+	const handleLogin = (data: { userId: string }) => {
+		mutate(data, {
+			onSuccess: (data) => {
+				setCookie("userId", data.userId);
+				navigate("/home")
+			}
+		});
+	}
+
 	return (
-		<div>
-			
-		</div>
+		<form onSubmit={handleSubmit(handleLogin)}>
+			<input {...register("userId")}/>
+			<button type={"submit"}>로그인</button>
+		</form>
 	);
 };
 
