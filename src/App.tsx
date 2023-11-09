b149053e0dbed9d23844cea576daf20359ec937c
bobby <rlatnguscjsw@gmail.com>
2023-11-10 00:35:18 +0900

    Feat : Router
    - HomePage Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import AuthRoutes from '@page/common/AuthRoutes.tsx';
+import HomePage from '@page/login/HomePage.tsx';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthRoutes/>}>
+          <Route path={"/home"} element={<HomePage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/page/login/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+	return (
+		<div>
+			111
+		</div>
+	);
+};
+
+export default HomePage;
