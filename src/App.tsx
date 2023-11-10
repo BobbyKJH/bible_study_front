4d6cd442b4af43aec29a40c985d72f3c8196496b
bobby <rlatnguscjsw@gmail.com>
2023-11-10 23:22:42 +0900

    Feat : Page Route
    - QT, PBS, MyPage 중첩 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+/** Page */
+import NewTestamentPage from '@page/bible/NewTestamentPage.tsx';
+import OldTestamentPage from '@page/bible/OldTestamentPage.tsx';
+
+import PbsPage from '@page/pbs/PbsPage.tsx';
+import PbsEditPage from '@page/pbs/PbsEditPage.tsx';
+import PbsCreatePage from '@page/pbs/PbsCreatePage.tsx';
+import PbsDetailPage from '@page/pbs/PbsDetailPage.tsx';
+
+import QtPage from '@page/qt/QtPage.tsx';
+import QtEditPage from '@page/qt/QtEditPage.tsx';
+import QtCreatePage from '@page/qt/QtCreatePage.tsx';
+import QtDetailPage from '@page/qt/QtDetailPage.tsx';
+
+import MyPage from '@page/myPage/MyPage.tsx';
+import MyGraphPage from '@page/myPage/MyGraphPage.tsx';
 /** Query-Client */
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 /** Atom */
@@ -24,7 +40,33 @@ const App: React.FC = () => {
             <Route path={"/"} element={<LoginPage/>} />
 
             <Route element={<AuthRoutes/>}>
-              <Route path={"/home"} element={<HomePage/>} />
+
+              <Route path={"/home"}>
+                <Route path={""} element={<HomePage/>}/>
+
+                <Route path={"pbs"}>
+                  <Route path={""} element={<PbsPage/>}/>
+                  <Route path={"create"} element={<PbsCreatePage/>}/>
+                  <Route path={"edit/:id"} element={<PbsEditPage/>}/>
+                  <Route path={":id"} element={<PbsDetailPage/>}/>
+                </Route>
+
+                <Route path={"qt"}>
+                  <Route path={""} element={<QtPage/>}/>
+                  <Route path={"create"} element={<QtCreatePage/>}/>
+                  <Route path={"edit/:id"} element={<QtEditPage/>}/>
+                  <Route path={":id"} element={<QtDetailPage/>}/>
+                </Route>
+
+                <Route path={"myPage"}>
+                  <Route path={""} element={<MyPage/>}/>
+                  <Route path={"graph"} element={<MyGraphPage/>}/>
+                </Route>
+
+                <Route path={"oldTestament"} element={<OldTestamentPage/>}/>
+                <Route path={"newTestament"} element={<NewTestamentPage/>}/>
+              </Route>
+
             </Route>
 
           </Routes>
