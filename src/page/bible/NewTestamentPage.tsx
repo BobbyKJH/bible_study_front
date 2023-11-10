9d3537e1b2000b41a28ca6526c2400426d236bba
bobby <rlatnguscjsw@gmail.com>
2023-11-10 21:22:46 +0900

    Feat : Testament Page
    - 구약 신약 페이지 생성

---
+++ src/page/bible/NewTestamentPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewTestamentPage: React.FC = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default NewTestamentPage;
---
+++ src/page/bible/OldTestamentPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OldTestamentPage: React.FC = () => {
+	return (
+		<div>
+			
+			</div>
+	);
+};
+
+export default OldTestamentPage;
