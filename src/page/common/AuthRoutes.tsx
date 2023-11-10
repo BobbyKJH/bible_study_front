c670f794c3cdb011befcb9d3f06369e07fffbba1
bobby <rlatnguscjsw@gmail.com>
2023-11-10 19:57:39 +0900

    Feat : Header, Sidebar
    - AuthRoutes Common Component

--- src/page/common/AuthRoutes.tsx
+++ src/page/common/AuthRoutes.tsx
@@ -1,15 +1,24 @@
 /** React */
+import Sidebar from '@components/sidebar/Sidebar.tsx';
 import React from 'react';
 import { Outlet, Navigate } from "react-router-dom";
 /** Cookie */
 import { getCookie } from '@/libs/cookie.ts';
+/** Components */
+import Header from '@components/header/Header.tsx';
+/** Style */
+import { AuthRoutesContainer } from '@page/common/AuthRoutes.styled.ts';
 
 const AuthRoutes: React.FC = () => {
 	return (
 		getCookie("userId") ?
-			<>
+			<AuthRoutesContainer>
+				<Header/>
+
+				<Sidebar/>
+
 				<Outlet/>
-			</>
+			</AuthRoutesContainer>
 			:
 			// @ts-ignore
 			<Navigate to={"/"} {...alert("로그인이 필요합니다.")}/>
