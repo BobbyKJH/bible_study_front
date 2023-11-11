6f518cff18f47d73fb5a1c7f50aea7e548a881d5
bobby <rlatnguscjsw@gmail.com>
2023-11-11 18:32:49 +0900

    Feat : AuthRoutes.styled
    - AuthRoutesContainer media Query

--- src/page/common/AuthRoutes.styled.ts
+++ src/page/common/AuthRoutes.styled.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const AuthRoutesContainer = styled.div`
 	margin-top: 64px;
 	margin-left: 230px;
+	
+	@media(max-width: 899px){
+		margin-left: 0;
+	}
 `
