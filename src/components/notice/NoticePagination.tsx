86f002eec6a03822135ea15ef567e2923fab9b0b
bobby <rlatnguscjsw@gmail.com>
2023-11-16 23:00:21 +0900

    Feat : 게시판 Pagination
    - 게시판 Pagination Component Create
    - NoticePagination Style

---
+++ src/components/notice/NoticePagination.tsx
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
+/** Libs */
+import pagination from '@/libs/pagination.ts';
+/** Style */
+import { Pagination } from '@mui/material';
+import { NoticePaginationContainer } from '@components/notice/NoticePagination.styled.ts';
+
+interface Props {
+	count: number;
+}
+
+const NoticePagination: React.FC<Props> = ({ count }) => {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const page = Number(searchParams.get("page"));
+
+	const handlePageChange = useCallback((_event: React.ChangeEvent<unknown>, value: number) => {
+		searchParams.set("page", String(value));
+		setSearchParams(searchParams);
+		window.scrollTo( { top:0, left: 0, behavior: "smooth" });
+	}, [searchParams])
+
+	return (
+		<NoticePaginationContainer>
+			<Pagination count={pagination(count)} page={Number(page)} onChange={handlePageChange}/>
+		</NoticePaginationContainer>
+	);
+};
+
+export default NoticePagination;
