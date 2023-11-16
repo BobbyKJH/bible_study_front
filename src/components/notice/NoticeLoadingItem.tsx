914cea3fa31d46fb9835add4cef380efedfdcaf5
bobby <rlatnguscjsw@gmail.com>
2023-11-16 22:30:48 +0900

    Feat : NotionLoadingItem
    - Notion Loading Item Component Create
    - Style Create

---
+++ src/components/notice/NoticeLoadingItem.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+/** Style */
+import { Avatar, ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
+import { NoticeLoadingItemSubTitle, NoticeLoadingItemTitle } from '@components/notice/NoticeLoadingItem.styled.ts';
+/** Icon */
+import { BiBible } from 'react-icons/bi';
+
+const NoticeLoadingItem: React.FC = () => {
+	const tempArr = Array.from({ length: 10 }, (_, idx) => idx);
+
+	return (
+		<>
+			{...tempArr.map((temp) => (
+				<ListItemButton key={temp}>
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar>
+								<BiBible/>
+							</Avatar>
+						</ListItemAvatar>
+
+						<ListItemText
+							primary={<NoticeLoadingItemTitle animation="wave" />}
+							secondary={<NoticeLoadingItemSubTitle animation="wave" />}
+						/>
+					</ListItem>
+				</ListItemButton>
+			))}
+		</>
+	);
+};
+
+export default NoticeLoadingItem;
