9724ce94d8a94c40895cd3ecbdda03db51879ec1
bobby <rlatnguscjsw@gmail.com>
2023-11-16 20:29:27 +0900

    Feat : NoticeItem
    - 게시판 Item Component 생성

---
+++ src/components/notice/NoticeItem.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+/** Libs */
+import { BibleBooks } from '@/libs/BibleBooks.ts';
+/** Style */
+import { ListItemButton, ListItem, ListItemAvatar, ListItemText, Avatar } from '@mui/material';
+/** Icon */
+import { BiBible, BiSolidBible } from 'react-icons/bi';
+
+interface Props {
+	book: string;
+	startVerse: number;
+	endVerse: number;
+	chapter: number;
+}
+
+const NoticeItem: React.FC<Props> = ({ book, startVerse, endVerse, chapter }) => {
+	/** 신약 구약 구분 하는 FC */
+	const bibleId = BibleBooks.find((books) => {
+		const testament = books.book === book;
+		
+		return testament
+	})
+
+	const BibleIcon = bibleId?.id as number
+
+	return (
+			<ListItemButton>
+				<ListItem>
+					<ListItemAvatar>
+						<Avatar>
+							{BibleIcon < 40 ? <BiBible/> : <BiSolidBible/>}
+						</Avatar>
+					</ListItemAvatar>
+
+					<ListItemText
+						primary={<p style={{fontWeight: 900, fontSize:"1rem"}}>{book}</p>}
+						secondary={`${chapter}장 ${startVerse} - ${endVerse} 절`}
+					/>
+				</ListItem>
+			</ListItemButton>
+	);
+}
+
+export default NoticeItem;
