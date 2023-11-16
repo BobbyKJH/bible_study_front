c86b8b721cfaa38fe814db89ee097244eb8ca702
bobby <rlatnguscjsw@gmail.com>
2023-11-16 22:32:18 +0900

    Style : AuthRoutes
    - AuthRoutes margin bottom 62px

--- src/page/common/AuthRoutes.styled.ts
+++ src/page/common/AuthRoutes.styled.ts
@@ -4,6 +4,8 @@ export const AuthRoutesContainer = styled.div`
 	position: relative;
 	margin-top: 64px;
 	margin-left: 230px;
+	margin-bottom: 62px;
+	//height: calc(100vh - 100px);
 	
 	@media(max-width: 899px){
 		margin-left: 0;
