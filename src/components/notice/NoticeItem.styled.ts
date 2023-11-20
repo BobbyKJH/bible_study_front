80371c82d287db951dc8f1944f9e4c3c69972b77
bobby <rlatnguscjsw@gmail.com>
2023-11-20 22:22:39 +0900

    Style : NoticeItem Style
    - Font Size 반응형 수정

--- src/components/notice/NoticeItem.styled.ts
+++ src/components/notice/NoticeItem.styled.ts
@@ -3,9 +3,15 @@ import styled from 'styled-components';
 export const NoticeItemTitle = styled.span`
 	font-weight: 900;
 	font-size: 1rem;
+  @media(max-width: 899px){
+    font-size: .9rem;
+  }
 `;
 
 export const NoticeItemSubTitle = styled.span`
 	font-weight: 700;
   font-size: .8rem;
+  @media(max-width: 899px){
+    font-size: .7rem;
+  }
 `
