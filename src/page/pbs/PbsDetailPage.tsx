3a205e0c9a2fb2db21c686b36ad28f384104eb52
bobby <rlatnguscjsw@gmail.com>
2023-11-22 01:07:43 +0900

    Feat : PbsDetailPage
    - DetailPage Params 에 따른 Data 기능 생성

--- src/page/pbs/PbsDetailPage.tsx
+++ src/page/pbs/PbsDetailPage.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import { usePBSDetailQuery } from '@/api/PBSQuery.ts';
 
 const PbsDetailPage: React.FC = () => {
 	const { id } = useParams();
+	const { data, isLoading } = usePBSDetailQuery(id as string);
+
+	if(isLoading) return <p>hello</p>
+
 	return (
 		<div>
-			detail {id}
+			{data.book}
 		</div>
 	);
 };
