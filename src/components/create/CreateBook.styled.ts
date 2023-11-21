ab8b39b17833283efa5c80e178abbf23ad156f3f
bobby <rlatnguscjsw@gmail.com>
2023-11-22 01:06:32 +0900

    Feat : Create Book
    - Pbs Create Book Component Create
    - Create Book Style Create

---
+++ src/components/create/CreateBook.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export const CreateBookComplete = styled(Autocomplete)`
+  fieldset{
+    border: none;
+  }
+`
---
+++ src/components/create/CreateBook.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+/** Array */
+import { BibleBook } from '@/libs/BibleBooks.ts';
+/** Type */
+import { UseFormSetValue } from 'react-hook-form';
+/** Style */
+import { TextField } from '@mui/material';
+import { CreateBookComplete } from '@components/create/CreateBook.styled.ts';
+
+interface Props {
+	setValue: UseFormSetValue<any>;
+	storage: string | null;
+}
+
+const CreateBook: React.FC<Props> = ({ setValue, storage }) => {
+	const [inputValue, setInputValue] = React.useState(storage === null ? "" : storage);
+	const [book, setBook] = React.useState<string | null>(storage);
+
+	useEffect(() => {
+		setValue("book", book)
+		console.log( book );
+	}, [book])
+
+	const handleChangeBook = (_event: any, newValue: string | null) => {
+		setBook(newValue);
+	};
+
+	/** 성경 검색 FC */
+	const handleSearchInput = (_event: React.SyntheticEvent, newInputValue: string) => {
+		setInputValue(newInputValue);
+	}
+
+
+	return (
+		<CreateBookComplete
+			value={book}
+			fullWidth
+			onChange={handleChangeBook}
+			inputValue={inputValue}
+			onInputChange={handleSearchInput}
+			options={BibleBook}
+			renderInput={(params) => <TextField {...params} label="성경" />}
+		/>
+	);
+}
+
+export default CreateBook;
