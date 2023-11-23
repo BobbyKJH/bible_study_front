b4bd9df4f08c03fa9d642cde7082de814644e159
bobby <rlatnguscjsw@gmail.com>
2023-11-23 23:07:07 +0900

    Feat : PbsDetailPage
    - Pbs Detail Page Create
    - Pbs Detail Page Style

---
+++ src/page/pbs/PbsDetailPage.styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Container } from '@mui/material';
+
+export const PbsDetailContainer = styled(Container)`
+	padding: 0;
+	box-sizing: border-box;
+	position: relative;
+`;
+
+export const PbsDetailTitleBox = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+	padding: 20px 14px 10px 14px;
+`
