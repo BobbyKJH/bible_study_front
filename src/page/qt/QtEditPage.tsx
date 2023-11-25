0a060d4b9fdb3eefd998e92afe53720e1ba8101b
bobby <rlatnguscjsw@gmail.com>
2023-11-25 14:47:43 +0900

    Feat : Qt Edit Page
    - Qt Edit Page Create

--- src/page/qt/QtEditPage.tsx
+++ src/page/qt/QtEditPage.tsx
@@ -1,12 +1,100 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
+/** Api */
+import { useEditQTMutation, useQTDetailQuery } from '@/api/QTQuery.ts';
+/** Hook */
+import { useForm } from 'react-hook-form';
+import useSnack from '@/hook/useSnack.ts';
+/** Cookie */
+import { getCookie } from '@/libs/cookie.ts';
+/** Component */
+import EditBook from '@components/edit/EditBook.tsx';
+import EditVerse from '@components/edit/EditVerse.tsx';
+import EditContent from '@components/edit/EditContent.tsx';
+import EditShowData from '@components/edit/EditShowData.tsx';
+import EditButtonGroup from '@components/edit/EditButtonGroup.tsx';
+/** Type */
+import Bible from '@type/Bible';
+/** Style */
+import { Button, Container } from '@mui/material';
 
 const QtEditPage: React.FC = () => {
 	const { id } = useParams();
+	const navigate = useNavigate();
+	/** Api */
+	const { data, isLoading } = useQTDetailQuery(id as string);
+	const { mutate } = useEditQTMutation();
+	/** Hook */
+	const { handleOpenSnack } = useSnack();
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors } } = useForm({
+		defaultValues: {
+			id: id,
+			userId: getCookie("userId"),
+			userName: sessionStorage.getItem("userName")
+		}
+	});
+
+	const handleEditPbs = (data: Bible.Edit) => {
+		mutate(data, {
+			onSuccess: () => {
+				handleOpenSnack({ message: "수정 완료", severity: "info" });
+				navigate(-1);
+			}
+		})
+	}
+
+	if(isLoading) return <div>edit</div>
+
 	return (
-		<div>
-			Edit : {id}
-		</div>
+		<Container sx={{ m :"80px auto",padding:0, boxSizing: "border-box" }}>
+			<form onSubmit={handleSubmit(handleEditPbs)}>
+				<div style={{ display:"flex", justifyContent: "space-between", alignItems: "center"}}>
+					<EditBook setValue={setValue} value={data.book}/>
+				</div>
+
+				<div style={{ display:"flex", justifyContent: "space-between", alignItems: "center"}}>
+					<EditShowData setValue={setValue} value={data.showData}/>
+
+					<div>
+						<EditVerse
+							register={register}
+							value={data.chapter}
+							name={"chapter"}
+							content={"장"}
+							errors={errors?.chapter?.message}
+						/>
+
+						<EditVerse
+							register={register}
+							value={data.startVerse}
+							name={"startVerse"}
+							content={"절"}
+							errors={errors?.startVerse?.message}
+						/>
+
+						<EditVerse
+							register={register}
+							value={data.endVerse}
+							name={"endVerse"}
+							content={"절"}
+							errors={errors?.endVerse?.message}
+						/>
+					</div>
+				</div>
+
+				<hr style={{marginTop: "10px", border: "1px solid black"}}/>
+
+				<EditContent register={register} value={data.content}/>
+
+				<EditButtonGroup>
+					<Button type={"submit"}>수정</Button>
+				</EditButtonGroup>
+			</form>
+		</Container>
 	);
 };
 
