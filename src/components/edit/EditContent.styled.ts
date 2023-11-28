f9ef17361e16bfe948522bf6c0587542050d1a37
bobby <rlatnguscjsw@gmail.com>
2023-11-29 01:31:41 +0900

    Feat : Edit Content
    - 수정 Content Component Create
    - 수정 Content Style Create

---
+++ src/components/edit/EditContent.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { TextField } from '@mui/material';
+
+export const EditContentMultiTextField = styled(TextField)`
+	fieldset{
+		border: none;
+	}
+	.MuiInputBase-input{
+		font-weight: 700;
+	}
+`
