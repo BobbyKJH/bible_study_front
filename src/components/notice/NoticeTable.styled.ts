dbc5deeac80d402004c7c04cd72fc92a552f903e
bobby <rlatnguscjsw@gmail.com>
2023-12-01 23:09:43 +0900

    Style : NoticeTable Style
    - Notice Table Style

--- src/components/notice/NoticeTable.styled.ts
+++ src/components/notice/NoticeTable.styled.ts
@@ -1,14 +1,14 @@
-import { TableCell, TableHead } from '@mui/material';
-import styled from 'styled-components';
+import { TableCell, TableHead } from "@mui/material";
+import styled from "styled-components";
 
 export const NoticeTableHeader = styled(TableHead)`
-	box-sizing: border-box;
-	border-radius: 10px;
-	background-color: #80808020;
-`
+  box-sizing: border-box;
+  border-radius: 10px;
+  background-color: #80808020;
+`;
 
 export const NoticeTableCell = styled(TableCell)`
-	font-weight: 700;
-	font-size: 1rem;
-	width: 10%;
-`
+  font-weight: 700;
+  font-size: 1rem;
+  width: 10%;
+`;
