30209ce96b30a516fb5b47a4f2c6f8cf27c4a84a
bobby <rlatnguscjsw@gmail.com>
2023-12-02 23:37:52 +0900

    Feat : Pbs, Qt Graph
    - Pbs Qt Graph 등록

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+/** Api */
+import { useAdminQuery } from "@/api/AdminQuery";
+/** Component */
+import PbsBible from "@/components/home/PbsBible";
+import QtStatisticsGraph from "@/components/graph/QtStatisticsGraph";
+import PbsStatisticsGraph from "@/components/graph/PbsStatisticsGraph";
 
 const HomePage: React.FC = () => {
-	return (
-		<div>
-			111
-		</div>
-	);
+  const { data, isLoading } = useAdminQuery();
+
+  if (isLoading) {
+    return <>Graph</>;
+  }
+
+  return (
+    <div>
+      <PbsBible />
+      <div style={{ display: "flex" }}>
+        <PbsStatisticsGraph startDate={data.startDate} endDate={data.endDate} />
+        <QtStatisticsGraph startDate={data.startDate} endDate={data.endDate} />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
