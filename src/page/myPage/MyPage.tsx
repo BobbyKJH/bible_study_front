b34550c253db9286b7526d6fb412d57d7f97349b
bobby <rlatnguscjsw@gmail.com>
2023-12-03 00:06:38 +0900

    Feat : MyPage
    - Recoil State 추가

--- src/page/myPage/MyPage.tsx
+++ src/page/myPage/MyPage.tsx
@@ -1,9 +1,24 @@
+import { MyPageNotice } from '@/atom/MyPageAtom.ts';
+import MyPageGraph from '@components/myPage/MyPageGraph.tsx';
+import MyPageTabs from '@components/myPage/MyPageTabs.tsx';
 import React from 'react';
+import MyQtNotice from '@components/myPage/MyQtNotice.tsx';
+import MyPbsNotice from '@components/myPage/MyPbsNotice.tsx';
+import { useRecoilValue } from 'recoil';
 
 const MyPage: React.FC = () => {
+	const notice = useRecoilValue(MyPageNotice);
+
+
 	return (
 		<div>
-			
+			<MyPageTabs/>
+
+			{notice === "pbs" && <MyPbsNotice/>}
+
+			{notice === "qt" && <MyQtNotice/>}
+
+			{notice === "graph" && <MyPageGraph/>}
 		</div>
 	);
 };
