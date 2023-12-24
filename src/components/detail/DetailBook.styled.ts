745dc271583afc8ededc1f0bd2e6ab29e192b7fd
bobby <rlatnguscjsw@gmail.com>
2023-12-25 00:23:58 +0900

    Style : DetailBook Style
    - Font size Font Weight 변경

--- src/components/detail/DetailBook.styled.ts
+++ src/components/detail/DetailBook.styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const DetailTitle = styled.h1`
-	font-size: 1.5rem;
-	font-weight: 900;
+	font-size: 1.3rem;
+	font-weight: 700;
+	padding: 30px 10px;
 `
