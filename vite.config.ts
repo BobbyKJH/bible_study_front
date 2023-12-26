245180aed8a9f58d5b564dd2b5d7b3dfdb335c26
bobby <rlatnguscjsw@gmail.com>
2023-12-27 00:31:46 +0900

    Feat : 절대 경로 추가
    - hook File 절대 경로 추가
    - atom File 절대 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
       { find: "@page", replacement: "/src/page" },
       { find: "@libs", replacement: "/src/libs" },
       { find: "@components", replacement: "/src/components" },
-      { find: "@api", replacement: "/src/api" }
+      { find: "@api", replacement: "/src/api" },
+      { find: "@hook", replacement: "/src/hook" },
+      { find: "@atom", replacement: "/src/atom" }
     ],
   },
 })
