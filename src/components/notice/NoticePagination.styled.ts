30660e306bcc2d9d84c044e34cc278504be4a269
bobby <rlatnguscjsw@gmail.com>
2023-12-27 23:20:58 +0900

    Style : Notice Pagination Style
    - left 0 수정

--- src/components/notice/NoticePagination.styled.ts
+++ src/components/notice/NoticePagination.styled.ts
@@ -3,14 +3,11 @@ import styled from 'styled-components';
 export const NoticePaginationContainer = styled.div`
 	position: fixed;
 	bottom: 0;
-	left: 200px;
+	left: 0;
 	right: 0;
 	display: flex;
   padding: 15px 0;
 	justify-content: center;
 	background-color: #fff;
 	box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
-  @media(max-width: 899px){
-    left: 0;
-  }
 `;
