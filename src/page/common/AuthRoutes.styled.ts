750167fcc7ab7159a70d0ec00f4b442d30990d4d
bobby <rlatnguscjsw@gmail.com>
2023-12-27 22:43:16 +0900

    Style : AuthRouter Style
    - AuthRouterContainer margin 변경

--- src/page/common/AuthRoutes.styled.ts
+++ src/page/common/AuthRoutes.styled.ts
@@ -2,11 +2,5 @@ import styled from 'styled-components';
 
 export const AuthRoutesContainer = styled.div`
 	position: relative;
-	margin-top: 64px;
-	margin-left: 200px;
-	margin-bottom: 62px;
-	
-	@media(max-width: 899px){
-		margin-left: 0;
-	}
+	margin-bottom: 62px;	
 `
