ff0fc4b5bf28d5d8abd69e676e1fb83cb32d661b
bobby <rlatnguscjsw@gmail.com>
2024-01-02 17:05:08 +0900

    Feat : UserQuery
    - User Query Http path

--- src/api/UserQuery.ts
+++ src/api/UserQuery.ts
@@ -4,7 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 /** User 조회 */
 const getUserId = async (data: { userId: string }) => {
   try {
-    const res = await AxiosInstance.post(`users/userId`, data);
+    const res = await AxiosInstance.post(`user/login`, data);
 
     return res.data;
   } catch (err) {
@@ -17,7 +17,7 @@ export const useUserMutation = () => useMutation({ mutationFn: getUserId });
 /** User 생성 */
 const createUserId = async (data: { userName: string; userId: string, auth: string }) => {
   try {
-    const res = await AxiosInstance.post(`users`, data);
+    const res = await AxiosInstance.post(`user`, data);
 
     return res.data;
   } catch (err) {
