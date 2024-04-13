636221e361022273e1dd882d8486bdada9ec89f2
bobby <rlatnguscjsw@gmail.com>
2024-04-13 21:08:20 +0900

    Feat : Next Install
    - Next JS Install

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  )
+};
+
+export default HomePage
